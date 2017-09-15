7ed09776b0e0ffff2a1d4496b95884681d13eeb0
NamrathaS <31969433+NamrathaS@users.noreply.github.com>
2017-09-15 09:58:27 +0530

    Rename test_bench.v to test_bench1.v

---
+++ test_bench1.v
@@ -0,0 +1,25 @@
+`include "nrd.v"
+module non_restoring_divider_tb;
+//inputs
+reg[3:0] X;  
+reg[3:0] Y; 
+//outputs
+wire[4:0] R;
+wire[3:0] Q; 
+non_restoring_divider uut (.X(X),.Y(Y));
+initial begin
+//intialise inputs
+  X=4'b0000; Y=4'b0000;
+  //reset
+  #200 X=4'b0110; Y=4'b0010;
+  #200 X=4'b1100; Y=4'b0011; 
+  #200 X=4'b1101; Y=4'b1100; 
+  #200 X=4'b0101; Y=4'b1010; 
+  #200 X=4'b1001; Y=4'b1100; 
+  #200 X=4'b1110; Y=4'b1001; 
+end
+initial begin
+	$dumpfile("nrd.vcd");
+	$dumpvars();
+end
+endmodule
